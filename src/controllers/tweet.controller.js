181319c1d9dd50561b9b3378e6024279fa9fd9cb
ddas4548 <ddas4548@gmail.com>
2024-10-18 12:55:45 +0530

    createTweet controller is updated

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,6 +7,38 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    //Extract tweet content
+    //Validate tweet content:
+    //Check if user exists
+    //Create and save the tweet to DB
+    //Respond with the created tweet
+
+    const {content} = req.body
+
+     // Validation - check if tweetContent is not empty
+     if (!content || content.trim() === "") {
+        throw new ApiError(400, "Tweet content is required.");
+    }
+
+    // Validation - check if user exists
+    const user = await User.findById(req.user._id);
+    if (!user) {
+        throw new ApiError(404, "User not found.");
+    }
+
+    // Create and save the tweet to DB    
+    const tweet = await Tweet.create({
+        content,
+        owner: user._id
+    })
+
+    // Respond with the created tweet    
+    res.status(201).json({
+        success: true,
+        message: "Tweet created successfully.",
+        data: tweet,
+    
+    })
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
